eace0437b7f6f215cead2492199f7307d9c11823
toan1231 <chauthai2003@gmail.com>
2022-11-29 08:51:40 -0500

    16

--- src/test/java/ui_automation/step_definitions/HrmOrangeStep.java
+++ src/test/java/ui_automation/step_definitions/HrmOrangeStep.java
@@ -30,7 +30,7 @@ public class HrmOrangeStep {
         String username=ConfigurationReader.getProperty("ui-config.properties","hrmOrange.username");
         String password=ConfigurationReader.getProperty("ui-config.properties","hrmOrange.password");
         loginPage.login(username,password);
-        int a=15;
+        int a=16;
         //ti bo cai nay di khong xao het
 
 
